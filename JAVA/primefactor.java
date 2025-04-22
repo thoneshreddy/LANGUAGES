09e37d3ce7d5f195f00a6b5a20691cfa6379a14e
thoneshreddy <rthonesh@gmail.com>
2025-04-22 19:54:28 +0530

    primefactor.java

--- JAVA/primefactor.java
+++ JAVA/primefactor.java
@@ -3,19 +3,18 @@ class Primefact {
 public static void prime(int n)
 {
 if(n==1)
-return;
-int i=2;
-while(n%i!=0)
-{
-i++;
-}
+    return;
+    int i=2;
+    while(n%i!=0)
+    {
+        i++;
+    }
 System.out.print(i+" ");
 prime(n/i);
 }
-
-
-    public static void main(String[] args) {
-      int n=200;
-prime(n);
-    }
+public static void main(String[] args) 
+  {
+    int n=200;
+        prime(n);
+  } 
 }
